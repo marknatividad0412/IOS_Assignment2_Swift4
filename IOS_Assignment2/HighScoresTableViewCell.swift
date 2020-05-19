5a4e06a0ed0964b314b026330e461fc7dd9caa6d
Mark Natividad <marknatividad@gmail.com>
2020-05-19 11:42:40 +1000

    Change to swift version 4

---
+++ IOS_Assignment2/HighScoresTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HighScoresTableViewCell.swift
+//  IOS_Assignment2
+//
+//  Created by Mark Anthony Natividad on 19/5/20.
+//  Copyright © 2020 Mark Anthony Natividad. All rights reserved.
+//
+
+import UIKit
+
+class HighScoresTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
